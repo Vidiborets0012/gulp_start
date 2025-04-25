c4445c823bb33e0f9e7d2b1a6c9e245b1ead6856
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-25 13:05:48 +0300

    add a function for processing SCSS styles, specify the path to the main SCSS file, the source folder for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,7 @@ const { src, dest, watch, series, parallel } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+
+function styles() {
+  return src('app/scss/style.scss').pipe(scss()).pipe(dest('app/css')).pipe(browserSync.stream());
+}
